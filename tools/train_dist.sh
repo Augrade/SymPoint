7482e55adfd617622d1b87223c2e219156b0dfa9
Kaiwalya Shukla <31895599+Kaiwalya2502@users.noreply.github.com>
2025-06-10 14:15:32 -0700

    Update train_dist.sh

--- tools/train_dist.sh
+++ tools/train_dist.sh
@@ -4,21 +4,21 @@ export PYTHONPATH=./
 GPUS=1
 
 # For remapped classes training
-OMP_NUM_THREADS=$GPUS torchrun --nproc_per_node=$GPUS --master_port=$((RANDOM + 10000)) tools/train.py \
+OMP_NUM_THREADS=$GPUS /home/kai/miniconda3/envs/spv1/bin/torchrun --nproc_per_node=$GPUS --master_port=$((RANDOM + 10000)) tools/train.py \
 	configs/svg/svg_pointT_remapped.yaml \
 	--dist \
 	--exp_name remapped_classes \
 	--sync_bn
 
 # For training without railing (uncomment to use)
-# OMP_NUM_THREADS=$GPUS torchrun --nproc_per_node=$GPUS --master_port=$((RANDOM + 10000)) tools/train.py \
+# OMP_NUM_THREADS=$GPUS /home/kai/miniconda3/envs/spv1/bin/torchrun --nproc_per_node=$GPUS --master_port=$((RANDOM + 10000)) tools/train.py \
 # 	configs/svg/svg_pointT_no_railing.yaml \
 # 	--dist \
 # 	--exp_name no_railing \
 # 	--sync_bn
 
 # For original training (uncomment to use)
-# OMP_NUM_THREADS=$GPUS torchrun --nproc_per_node=$GPUS --master_port=$((RANDOM + 10000)) tools/train.py \
+# OMP_NUM_THREADS=$GPUS /home/kai/miniconda3/envs/spv1/bin/torchrun --nproc_per_node=$GPUS --master_port=$((RANDOM + 10000)) tools/train.py \
 # 	configs/svg/svg_pointT.yaml \
 # 	--dist \
 # 	--exp_name baseline_original \
