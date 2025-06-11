023604b4fa3e26ed73e60d6f34c476085cae6b0c
Kaiwalya Shukla <31895599+Kaiwalya2502@users.noreply.github.com>
2025-06-11 11:44:05 -0700

    reduce mem usage

--- tools/train_dist.sh
+++ tools/train_dist.sh
@@ -4,11 +4,13 @@ export PYTHONPATH=./
 GPUS=8
 
 # For remapped classes training
+# To resume training, add: --resume /path/to/checkpoint.pth
 OMP_NUM_THREADS=$GPUS /home/kai/miniconda3/envs/spv1/bin/torchrun --nproc_per_node=$GPUS --master_port=$((RANDOM + 10000)) tools/train.py \
 	configs/svg/svg_pointT_remapped.yaml \
 	--dist \
 	--exp_name remapped_classes \
-	--sync_bn
+	--sync_bn \
+	--resume /home/kai/SymPoint/work_dirs/svg/svg_pointT_remapped/remapped_classes/best.pth
 
 # For training without railing (uncomment to use)
 # OMP_NUM_THREADS=$GPUS /home/kai/miniconda3/envs/spv1/bin/torchrun --nproc_per_node=$GPUS --master_port=$((RANDOM + 10000)) tools/train.py \
